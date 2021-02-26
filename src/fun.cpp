2ef91d4eded22a82c1ea21cc2cc96ef047ca624c
=TheNameless0ne <=alexfinsher@gmail.com>
2021-02-26 17:11:08 +0300

    Реализовал функцию возведения в степень -Назаров М.А.

---
+++ src/fun.cpp
@@ -0,0 +1,10 @@
+#include "fun.h"
+
+long long power(long long x, unsigned short n)
+{
+	long long res = x;
+	for (int i = 1; i < n; ++i) {
+		res *= res;
+	}
+	return res;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "fun.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << power(2, 4);
+	return 0;
+}
